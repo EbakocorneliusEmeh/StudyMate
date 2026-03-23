a4721255f7de19f4d6fbe029cdc0d0d29da2ac42
Your Name <you@example.com>
2026-03-23 11:54:45 +0100

    fix syntax errors

--- src/api/sessions.ts
+++ src/api/sessions.ts
@@ -1,7 +1,7 @@
 const BACKEND_URL =
   typeof window !== 'undefined' && window.location.hostname === 'localhost'
     ? 'http://localhost:3000'
-    : process.env.EXPO_PUBLIC_API_URL || 'http://192.168.1.172:3000';
+    : process.env.EXPO_PUBLIC_API_URL || 'http://172.20.10.5:3000';
 
 export interface StudySession {
   id: string;
